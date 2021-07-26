274d6b4d0eb2d6c8f4cd58f9d8ff16df030e186e
LAPTOP-JUB29097\User PC <bhaveshkumar.natvarl@petronas.com>
2021-07-26 18:52:34 +0800

    https

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-const baseDomain = 'http://api.additivasia.io/api/v1/assignment/employees/';
+const baseDomain = 'https://api.additivasia.io/api/v1/assignment/employees/';
 
 export const baseUrl = `${baseDomain}`;
