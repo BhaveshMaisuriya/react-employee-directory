96ac3eb64263ab9dcf17667c09e7d48616081cc8
LAPTOP-JUB29097\User PC <bhaveshkumar.natvarl@petronas.com>
2021-07-26 18:46:50 +0800

    tets

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from "./components/Layout";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SubEmployee from "./components/SubEmployee";
 
 function App() {
