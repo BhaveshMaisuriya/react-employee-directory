f6b6a443bf2eee11149a64722fa9685b481f6674
LAPTOP-JUB29097\User PC <bhaveshkumar.natvarl@petronas.com>
2021-07-26 20:18:01 +0800

    Updates

---
+++ precache-manifest.980669a0e5625704d24c3a837d1df651.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf489a5f410d7c6435432ef802e4e23",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "30a008298ab625cb7d1b",
+    "url": "/react-employee-directory/static/css/main.f6a42cf4.chunk.css"
+  },
+  {
+    "revision": "e4a443fec20d791bfb9c",
+    "url": "/react-employee-directory/static/js/2.a7986aa5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-employee-directory/static/js/2.a7986aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30a008298ab625cb7d1b",
+    "url": "/react-employee-directory/static/js/main.e8124aa0.chunk.js"
+  },
+  {
+    "revision": "8e632fc8d7abb2283eb9",
+    "url": "/react-employee-directory/static/js/runtime-main.c8d5e7ba.js"
+  },
+  {
+    "revision": "4788048f319dc48101678d9e69f5077e",
+    "url": "/react-employee-directory/static/media/employee.4788048f.png"
+  }
+]);
